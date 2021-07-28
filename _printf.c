8a99e9105d4f0ed7f6e28d7dc7f6d358216a96d1
Andrea Angola <3469@holbertonschool.com>
2021-07-28 14:26:29 -0700

    upload file with function _printf

---
+++ _printf.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ * _printf - llamar a diferentes funciones
+ * @format: puntero
+ * Return: Always 0.
+ */
+
+int _printf(const char *format, ...)
+{
+	int i;
+	int cont = 0;
+
+	va_list arg;
+
+	va_start(arg, format);
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '%')
+			{
+				_putchar(37);
+				i++;
+			}
+
+			else if (get(format, i + 1) != NULL)
+			{
+				cont += (get(format, i + 1))(arg);
+				i++;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			cont++;
+		}
+	}
+	va_end(arg);
+	return (cont);
+}
