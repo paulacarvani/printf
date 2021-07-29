86f15a2e9c7a31e8ec82ea8c6ed42cde588a5d1f
paulacarvani <3312@holbertonschool.com>
2021-07-29 14:23:57 -0700

    Adds functions of task 1

--- 01functions.c
+++ 01functions.c
@@ -28,26 +28,3 @@ _putchar(s[i]);
 }
 return (i);
 }
-/**
- * print_int - Entry point
- *@list: list
- * Return: Always 0
- */
-int print_int(va_list list)
-{
-int i = va_arg(list, int);
-_putchar(i % 10 + '0');
-printf("AQUI ESTA %i\n", i);
-return (0);
-}
-/**
- * print_decimal - Entry point
- *@list: list
- * Return: Always 0
- */
-int print_decimal(va_list list)
-{
-int d = va_arg(list, int);
-_printinteger(d);
-return (0);
-}
