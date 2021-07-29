d47d93193379f9d045194102ac5d3a6808f13203
Andrea Angola <3469@holbertonschool.com>
2021-07-28 22:16:30 -0700

    get the recursive fuction and fuction %d

--- 01functions.c
+++ 01functions.c
@@ -29,11 +29,29 @@ _putchar(s[i]);
 }
 return (i);
 }
+/**
+ * print_int - Entry point
+ *
+ * Return: Always 0
+ */
 int print_int(va_list list)
 {
 	int i = va_arg(list, int);
+
 	_putchar(i % 10 + '0');
-	printf("AQUI ESTA %i\n",i);
-	return(0);
-	
+	printf("AQUI ESTA %i\n", i);
+	return (0);
+}
+/**
+ * print_decimal - Entry point
+ *
+ * Return: Always 0
+ */
+int print_decimal(va_list list)
+{
+	int d = va_arg(list, int);
+
+	_printinteger(d);
+
+	return (0);
 }
---
+++ _printinteger.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _printinteger - recursive fuction
+ * @n: variable
+ * Return: Always 0
+ */
+void _printinteger(long n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		_printinteger(n / 10);
+		_putchar(n % 10 + '0');
+	}
+}
