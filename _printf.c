9717ffb0faea23132b6efc1721ae869086deda69
paulacarvani <3312@holbertonschool.com>
2021-07-28 20:53:19 -0700

    Update the var cont

--- _printf.c
+++ _printf.c
@@ -7,34 +7,32 @@
 
 int _printf(const char *format, ...)
 {
-	int i;
-	int cont = 0;
-
-	va_list arg;
-
-	va_start(arg, format);
-	for (i = 0; format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == '%')
-			{
-				_putchar(37);
-				i++;
-			}
-
-			else if (get(format, i + 1) != NULL)
-			{
-				cont += (get(format, i + 1))(arg);
-				i++;
-			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			cont++;
-		}
-	}
-	va_end(arg);
-	return (cont);
+int i;
+int cont = 0;
+va_list arg;
+va_start(arg, format);
+for (i = 0; format[i]; i++)
+{
+if (format[i] == '%')
+{
+if (format[i + 1] == '%')
+{
+_putchar(37);
+cont++;
+i++;
+}
+else if (get(format, i + 1) != NULL)
+{
+cont += (get(format, i + 1))(arg);
+i++;
+}
+}
+else
+{
+_putchar(format[i]);
+cont++;
+}
+}
+va_end(arg);
+return (cont);
 }
